1c103144235ca13d5eff5e93da70911d7ac064a6
MSS <MSS@d.c>
2024-07-31 13:07:31 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client";
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+    Link,
+} from "react-router-dom";
+import Home from "./pages/home/page";
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home/>
+    },
+
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
